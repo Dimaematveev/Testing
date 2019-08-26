fa7e608726b8f7b1f79f9006cf219b36a9aa0434
dimaematveev <dimaematveev@gmail.com>
2019-08-26 22:56:09 +0300

    Контроллер думаю!

---
+++ TestingProject/TestingProject.BL/Controller/QuestionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingProject.BL.Model;
+
+namespace TestingProject.BL.Controller
+{
+    /// <summary>
+    /// Контроллер для вопроса
+    /// </summary>
+    public class QuestionController
+    {
+        
+
+        /// <summary>
+        /// Список вопросов.
+        /// </summary>
+        public List<Question> Questions { get; }
+
+        public QuestionController(string questionText,
+                                  List<Answer> answerOptions,
+                                  List<Answer> rightAnswers)
+        {
+            Questions.Add(new Question(questionText, answerOptions, rightAnswers));
+        }
+
+
+    }
+}
--- TestingProject/TestingProject.CMD/Program.cs
+++ TestingProject/TestingProject.CMD/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestingProject.BL.Controller;
 using TestingProject.BL.Model;
 
 namespace TestingProject.CMD
@@ -26,65 +27,70 @@ namespace TestingProject.CMD
                 new Answer("Ответ_1"),
                 new Answer("Ответ_5")
             };
-            Question question = new Question("Ответ 1,5!", answersAll, answersTrue);
+            QuestionController questions = new QuestionController("Ответ 1,5!", answersAll, answersTrue);
 
-            int[] numAns = { 0 };
-            int numLine;
-            do
+            foreach (var question in questions.Questions)
             {
+
+
+                int[] numAns = { 0 };
+                int numLine;
+                do
+                {
+                    Console.WriteLine(question.QuestionText);
+                    numLine = 1;
+                    foreach (var answer in question.AnswerOptions)
+                    {
+                        if (numAns.Contains(numLine))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        Console.WriteLine("\t" + numLine++ + "-" + answer.QuestionText);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+
+                    Console.WriteLine("Введите номер правильного ответа или ответов через запятую!(1,4)\nЛибо напишите y для подтверждения ответов!\nИ нажмите Enter!");
+                    string ans = Console.ReadLine();
+
+                    if (ans == "y")
+                    {
+                        break;
+                    }
+                    numAns = ans.Split(',').Select<string, int>(x => Convert.ToInt32(x)).ToArray();
+                    Console.Clear();
+                    Console.WriteLine("Желтым то что выбрали!");
+                } while (true);
+
+
                 Console.WriteLine(question.QuestionText);
                 numLine = 1;
                 foreach (var answer in question.AnswerOptions)
                 {
                     if (numAns.Contains(numLine))
                     {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        if (answersTrue.Contains(answer))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                    }
+                    else
+                    {
+                        if (answersTrue.Contains(answer))
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
                     }
                     Console.WriteLine("\t" + numLine++ + "-" + answer.QuestionText);
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
 
-                Console.WriteLine("Введите номер правильного ответа или ответов через запятую!(1,4)\nЛибо напишите y для подтверждения ответов!\nИ нажмите Enter!");
-                string ans = Console.ReadLine();
-                
-                if (ans=="y")
-                {
-                    break;
                 }
-                numAns = ans.Split(',').Select<string, int>(x => Convert.ToInt32(x)).ToArray();
-                Console.Clear();
-                Console.WriteLine("Желтым то что выбрали!");
-            } while (true);
-
-
-            Console.WriteLine(question.QuestionText);
-            numLine = 1;
-            foreach (var answer in question.AnswerOptions)
-            {
-                if (numAns.Contains(numLine))
-                {
-                    if (answersTrue.Contains(answer))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                    }
-                }
-                else
-                {
-                    if (answersTrue.Contains(answer))
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                }
-                Console.WriteLine("\t" + numLine++ + "-" + answer.QuestionText);
-                
             }
             Console.ForegroundColor = ConsoleColor.White;
             Console.ReadLine();
